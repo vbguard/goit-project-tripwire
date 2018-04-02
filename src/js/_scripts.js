3a57a782283ac5c9f6ee6e28c0fbfb6995848607
Viktor <admin@rizne.in.ua>
2018-04-02 04:07:13 +0300

    add section 1-4 for 320 media & js for slider

---
+++ src/js/_scripts.js
@@ -0,0 +1,14 @@
+// on slider
+$(document).ready(function(){
+  $('.slider-comment').slick({
+    dots: true
+  });
+});
+
+$('.fade').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
+});
